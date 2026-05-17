23426cb453bd5f4c64dc8c9f16d002ebd061cc3f
AnmolMehra435 <gtrdemon66@gmail.com>
2026-05-17 20:48:37 +0530

    updated backend ports

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,8 @@ const verifyJwt = require("./middleware/verifyJwt")
 require('dotenv').config()
 const app = express();
 
+const port = process.env.PORT || 5000
+
 app.use(cors());
 app.use(express.json());
 
@@ -17,7 +19,7 @@ const connectDB = async () => {
 
         console.log("MongoDB connected....")
 
-        app.listen(5000, () => {
+        app.listen(port, () => {
             console.log("Server is running...")
         })
     }catch(err){
